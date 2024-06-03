44336a7d3ac7564e68939d702c5daf41fd6082e7
TontonBanton <pancla13ulas@gmail.com>
2024-06-03 10:53:06 +0800

    reconfig sequelize attributes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config.js')[process.env.NODE_ENV || 'development'];
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+//const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize (config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+  dialectModule: require('mysql2')
+})
 
 const db = {
   sequelize,
