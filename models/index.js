8b4b991af22722f9bd260c0027262fbecb0c2e38
TontonBanton <pancla13ulas@gmail.com>
2024-06-18 16:18:26 +0800

    Sequelize config edit

--- models/index.js
+++ models/index.js
@@ -1,16 +1,46 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config.js')[process.env.NODE_ENV || 'development'];
 
-const sequelize = new Sequelize (config.database, config.username, config.password, {
+const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: 'mysql',
-  dialectModule: require('mysql2')
-})
+  dialectModule: require('mysql2'),
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  dialectOptions: {
+    connectTimeout: 60000
+  },
+  retry: {
+    max: 5,
+    match: [
+      /ETIMEDOUT/,
+      /EHOSTUNREACH/,
+      /ECONNREFUSED/,
+      /ECONNRESET/,
+      /ENOTFOUND/
+    ],
+    backoffBase: 1000,
+    backoffExponent: 1.5
+  },
+  logging: console.log // Add this line to log Sequelize operations
+});
 
 const db = {
   sequelize,
   Sequelize,
   User: require('./user')(sequelize, Sequelize),
 };
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = db;
