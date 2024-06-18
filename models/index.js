67e63e81887da002a0035fdcb659380374460c53
TontonBanton <pancla13ulas@gmail.com>
2024-06-18 17:15:23 +0800

    config db edit

--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config.js')[process.env.NODE_ENV || 'development'];
 
-const sequelize = new Sequelize (config.database, config.username, config.password, {
+const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
+  port: config.port,
   dialect: 'mysql',
-  dialectModule: require('mysql2')
-})
+  dialectModule: require('mysql2'),
+  pool: config.pool,
+  dialectOptions: config.dialectOptions,
+  retry: config.retry,
+  logging: console.log, // Enable logging for debugging purposes
+});
 
 const db = {
   sequelize,
   Sequelize,
-  User: require('./user')(sequelize, Sequelize),
+  User: require('./user')(sequelize, Sequelize), // Adjust the path to your user model
 };
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err.message);
+    console.error(err.stack);
+    console.error('Parent Error:', err.parent ? err.parent.message : 'N/A');
+  });
 
 module.exports = db;
