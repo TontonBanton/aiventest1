22baf12e9e4d319e916368623da44fe7b3285795
TontonBanton <pancla13ulas@gmail.com>
2024-06-18 16:56:42 +0800

    config back to default

--- models/index.js
+++ models/index.js
@@ -1,28 +1,16 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config.js')[process.env.NODE_ENV || 'development'];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
+const sequelize = new Sequelize (config.database, config.username, config.password, {
   host: config.host,
-  dialect: config.dialect,
-  dialectModule: config.dialectModule,
-  pool: config.pool,
-  dialectOptions: config.dialectOptions,
-  retry: config.retry,
-  logging: console.log // Add this to log Sequelize operations
-});
+  dialect: 'mysql',
+  dialectModule: require('mysql2')
+})
 
 const db = {
   sequelize,
   Sequelize,
-  User: require('./user')(sequelize, Sequelize), // Adjust the path to your user model
+  User: require('./user')(sequelize, Sequelize),
 };
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 module.exports = db;
