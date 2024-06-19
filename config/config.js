fdff47f32f39f299eb6995a14c335a252a878d96
TontonBanton <pancla13ulas@gmail.com>
2024-06-19 17:23:41 +0800

    config2 cchange

--- config/config.js
+++ config/config.js
@@ -13,8 +13,29 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // Default to 3306 if DB_PORT is not set
     dialect: 'mysql',
-    dialectModule: require('mysql2')
+    dialectModule: require('mysql2'),
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      connectTimeout: 60000, // 60 seconds
+    },
+    retry: {
+      max: 5,
+      match: [
+        /ETIMEDOUT/,
+        /EHOSTUNREACH/,
+        /ECONNREFUSED/,
+        /ECONNRESET/,
+        /ENOTFOUND/
+      ],
+      backoffBase: 1000, // Initial backoff duration in ms
+      backoffExponent: 1.5, // Exponential backoff factor
+    },
   },
 };
-
