de08d0934872da1a86bce510176aa2de4eda8f43
TontonBanton <pancla13ulas@gmail.com>
2024-06-26 15:22:43 +0800

    mod0626322

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306, // Default to 3306 if DB_PORT is not set
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     dialectModule: require('mysql2')
   },
