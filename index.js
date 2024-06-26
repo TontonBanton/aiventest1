f82a1db973e05cad342074c591f44dc99c0ec7f9
TontonBanton <pancla13ulas@gmail.com>
2024-06-26 13:34:24 +0800

    mod0626134

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use('/api', routes);
 //Database
 const { sequelize } = require('./models');
 const PORT = process.env.PORT || 3001;
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
